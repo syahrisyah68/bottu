8629b5bf6afee6ba8cec85834e52b9820232b161
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-05 14:38:36 +0800

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/FahriAdison/Sad-Multi-Device*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+let str = `*https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan Subscribe Channel saya dibawah ,Terimakasih`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
