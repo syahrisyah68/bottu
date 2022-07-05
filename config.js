b2f27fd7da1f9025c864a49007e7b2d821eff474
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-05 18:42:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -64,10 +64,10 @@ global.flaaa = [
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
  ]
 global.sgc = 'https://chat.whatsapp.com/KiKVjpgL4EH6i0kMUtlEJz'
-global.sig = '"_"'
+global.sig = 'https://www.instagram.com/bot_whangsaf'
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = ""_""
+global.urlnya = "https://www.instagram.com/bot_whangsaf"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
