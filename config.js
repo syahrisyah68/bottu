64c41a971e4b0f2cb39f4c51f0789b64fc09bc79
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-07-07 22:53:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -84,7 +84,7 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6281214020357'
+global.nomorbot = '6285643841250'
 global.nomorown = '6281251549899'
 global.namebot = 'ᴅᴀɴɪʙᴏᴛᴢ_ᴏғᴄ'
 global.nameown = 'ᴅᴀɴɪ ᴏғғᴄɪᴀʟ👑'
